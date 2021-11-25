0d8848dcb2da297b26430347af7a7f3263d28fd4
Tomava <66073405+Tomava@users.noreply.github.com>
2021-11-25 18:54:14 +0200

    Add UI and readme

--- CryptoAnalyzer.py
+++ CryptoAnalyzer.py
@@ -1,6 +1,7 @@
 import urllib.request
 import json
 from datetime import datetime, timedelta
+import tkinter
 
 COIN_NAME = "bitcoin"
 CURRENCY = "eur"
@@ -31,13 +32,14 @@ class Backend:
         :param data: list
         :return: Datetime, int
         """
-        day = datetime.fromtimestamp(int(data[0]) / 1000).strftime("%Y-%m-%d")
+        # Convert from milliseconds to seconds
+        day = datetime.fromtimestamp(int(data[0]) / 1000).replace(hour=0, minute=0, second=0, microsecond=0)
         wanted_data = data[1]
         return day, wanted_data
 
     def fetch_data(self):
         """
-        Fetches data from coingecko with given parameters
+        Fetches data from coingecko api with given parameters
         :return: dict
         """
         url = f"https://api.coingecko.com/api/v3/coins/{self.coin_name}/market_chart/range?vs_currency=" \
@@ -46,7 +48,9 @@ class Backend:
         with urllib.request.urlopen(url) as site:
             fetched_data = json.loads(site.read())
         parsed_data = {}
+        # Remove duplicate data points for each day
         for key in fetched_data:
+            # Add empty list for each key
             parsed_data[key] = []
             iterated_days = set()
             for data in fetched_data.get(key):
@@ -69,7 +73,7 @@ class Backend:
         last_price = -1
         for data in fetched_data:
             day, price = self.parse_day_and_price(data)
-            # Don't add first day
+            # Compare to -1 to not add first day of timeframe
             if price < last_price != -1:
                 current_trend.append(data)
             if len(current_trend) > len(longest_trend):
@@ -95,7 +99,6 @@ class Backend:
     def get_best_days(self):
         """
         Calculate the best days to buy and sell in a given timeframe
-        O(n^2)
         :return: tuple, (Datetime, Datetime)
         """
         fetched_data = self.fetch_data().get("prices")
@@ -104,7 +107,7 @@ class Backend:
         # Compare every day to all the next
         for i, from_data in enumerate(fetched_data):
             from_day, from_price = self.parse_day_and_price(from_data)
-            for to_data in fetched_data[i:]:
+            for to_data in fetched_data[i + 1:]:
                 to_day, to_price = self.parse_day_and_price(to_data)
                 if to_price - from_price > highest_delta:
                     highest_delta = to_price - from_price
@@ -115,43 +118,100 @@ class Backend:
         """
         Parses command and parameters from input text
         :param input_text: str
-        :return: bool, Successful command
+        :return: str, Command output
         """
         input_parts = input_text.split(" ")
+        output_text = ""
+        if input_text.lower() == "quit" or input_text.lower() == "exit":
+            return "QUIT"
+        # 3 arguments needed
         if len(input_parts) != 3:
-            return False
+            return output_text
         command = input_parts[0].lower()
         start_date = datetime.fromisoformat(input_parts[1])
         end_date = datetime.fromisoformat(input_parts[2]) + timedelta(hours=1)
         self.set_dates(start_date, end_date)
+        # Handle commands (could use case in python 3.10)
         if command == "trend":
             longest_trend = self.get_downward_trend()
-            print(f"In {COIN_NAME}’s historical data from CoinGecko, the price decreased {len(longest_trend)} days in a"
-                  f" row for the inputs from {start_date.strftime('%Y-%m-%d')} and to {end_date.strftime('%Y-%m-%d')}")
+            output_text = f"In {COIN_NAME}’s historical data from CoinGecko, the price decreased {len(longest_trend)}" \
+                          f" days in a row for the inputs from {start_date.strftime('%Y-%m-%d')} and to " \
+                          f"{end_date.strftime('%Y-%m-%d')}"
         elif command == "highest":
             highest_volume = self.get_highest_volume()
-            print(f"In {COIN_NAME}’s historical data from CoinGecko, the highest trading volume was {highest_volume[1]} on " 
-                  f"{highest_volume[0]} for the inputs from {start_date.strftime('%Y-%m-%d')} and to "
-                  f"{end_date.strftime('%Y-%m-%d')}")
+            output_text = f"In {COIN_NAME}’s historical data from CoinGecko, the highest trading volume was " \
+                          f"{highest_volume[1]} on {highest_volume[0]} for the inputs from " \
+                          f"{start_date.strftime('%Y-%m-%d')} and to {end_date.strftime('%Y-%m-%d')}"
         elif command == "best_day":
             best_days = self.get_best_days()
             if len(best_days) == 0:
-                print(f"{COIN_NAME.title()} should not be bought (or sold) for the inputs from {start_date.strftime('%Y-%m-%d')} and"
-                      f" to {end_date.strftime('%Y-%m-%d')}")
+                output_text = f"{COIN_NAME.title()} should not be bought (or sold) for the inputs from " \
+                              f"{start_date.strftime('%Y-%m-%d')} and to {end_date.strftime('%Y-%m-%d')}"
             else:
-                print(f"Best pair of days to buy and sell {COIN_NAME} are {best_days[0]} and {best_days[1]} for the inputs"
-                      f" from {start_date.strftime('%Y-%m-%d')} and to {end_date.strftime('%Y-%m-%d')}")
-        return True
+                output_text = f"Best pair of days to buy and sell {COIN_NAME} are {best_days[0]} and " \
+                              f"{best_days[1]} for the inputs from {start_date.strftime('%Y-%m-%d')} and to " \
+                              f"{end_date.strftime('%Y-%m-%d')}"
+        return output_text
+
+
+class UI:
+    def __init__(self):
+        # Setting up ui
+        self.main_window = tkinter.Tk()
+        self.backend = Backend()
+        self.input_label_pane = tkinter.Frame(self.main_window)
+        self.input_label_pane.grid(row=0, column=0)
+        self.start_date_label = tkinter.Label(self.input_label_pane, text="Start date:")
+        self.end_date_label = tkinter.Label(self.input_label_pane, text="End date:")
+        self.start_date_label.pack(side=tkinter.LEFT, expand=True, fill=tkinter.BOTH)
+        self.end_date_label.pack(side=tkinter.RIGHT, expand=True, fill=tkinter.BOTH)
+
+        self.input_pane = tkinter.Frame(self.main_window)
+        self.input_pane.grid(row=1, column=0)
+        self.start_date_input = tkinter.Text(self.input_pane, height=1, width=10)
+        self.start_date_input.insert(tkinter.END, "2020-01-01")
+        self.start_date_input.pack(side=tkinter.LEFT, expand=True, fill=tkinter.BOTH)
+        self.end_date_input = tkinter.Text(self.input_pane, height=1, width=10)
+        self.end_date_input.insert(tkinter.END, "2020-12-31")
+        self.end_date_input.pack(side=tkinter.RIGHT, expand=True, fill=tkinter.BOTH)
+
+        self.trend_button = tkinter.Button(self.main_window, text="Longest downward trend",
+                                           command=lambda: self.activate("trend"))
+        self.highest_volume_button = tkinter.Button(self.main_window, text="Highest trading volume",
+                                                    command=lambda: self.activate("highest"))
+        self.best_days_button = tkinter.Button(self.main_window, text="Best days to buy and sell",
+                                               command=lambda: self.activate("best_day"))
+        self.trend_button.grid(row=2, column=0)
+        self.highest_volume_button.grid(row=3, column=0)
+        self.best_days_button.grid(row=4, column=0)
+        self.output_box = tkinter.Label(self.main_window, text="Output")
+        self.output_box.grid(row=5, column=0)
+        self.main_window.grid_columnconfigure(0, minsize=1000)
+
+        self.main_window.mainloop()
+
+    def activate(self, command_str):
+        output_text = self.backend.input_parser(f"{command_str} {self.start_date_input.get(1.0, 'end').strip()} "
+                                                f"{self.end_date_input.get(1.0, 'end').strip()}")
+        self.output_box.configure(text=output_text)
 
 
 def main():
+    ui = UI()
     backend = Backend()
-    backend.input_parser("trend 2020-01-19 2020-01-21")
-    backend.input_parser("trend 2020-03-01 2020-08-01")
-    backend.input_parser("highest 2020-01-19 2020-01-21")
-    backend.input_parser("highest 2020-03-01 2020-08-01")
-    backend.input_parser("best_day 2020-01-19 2020-01-21")
-    backend.input_parser("best_day 2020-03-01 2020-08-01")
+    while True:
+        output_text = backend.input_parser(input("> "))
+        if output_text == "QUIT":
+            print("Quitting")
+            break
+        else:
+            print(output_text)
+    # print(backend.input_parser("trend 2020-01-19 2020-01-21"))
+    # print(backend.input_parser("trend 2020-03-01 2021-08-01"))
+    # print(backend.input_parser("highest 2020-01-19 2020-01-21"))
+    # print(backend.input_parser("highest 2020-03-01 2021-08-01"))
+    # print(backend.input_parser("best_day 2020-01-19 2020-01-21"))
+    # print(backend.input_parser("best_day 2020-03-01 2021-08-01"))
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Vincit_Pre-assignment
- 
+
+## Requirements
+**Python 3.8**
+
+## Usage
+Running **CryptoAnalyzer.py** will start a GUI program with each wanted function as a separate button. Start and end dates can typed into the text boxes in ISO-format. If GUI is not wanted, program can be used with commands by just closing the GUI-window. The command line commands are as follows:
+- Longest downward trend:
+```
+trend <start_date> <end_date>
+trend 2020-01-19 2020-01-21
+trend 2020-03-01 2021-08-01
+```
+- Highest trading volume:
+```
+highest <start_date> <end_date>
+highest 2020-01-19 2020-01-21
+highest 2020-03-01 2021-08-01
+```
+- Best days to buy and sell:
+```
+best_day <start_date> <end_date>
+best_day 2020-01-19 2020-01-21
+best_day 2020-03-01 2021-08-01
+```
